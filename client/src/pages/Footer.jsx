768bcd2a68e0f8295a998c659f0a909763d08009
Gunjan Jalan <gunjanjalan13@gmail.com>
2025-12-05 18:48:04 +0530

    modified

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -3,12 +3,22 @@ import { FaCopyright } from "react-icons/fa";
 
 export default function  Footer() {
   return (
-    <div className="relative bottom-0 w-full">
-      <div className='mt-12 bottom-0 w-full bg-black h-10 flex bg-fixed items-center text-white gap-3'>
+    <div className="w-full bg-primary-900 text-white py-4 flex flex-col items-center">
+  
+  {/* Top Row */}
+  <div className="flex items-center gap-2">
+    <FaCopyright className="h-4 w-4" />
+    <span className="text-sm tracking-wide">Event Ease</span>
+  </div>
 
-        <FaCopyright className=" ml-24  h-5 "/>
-          Evento EMS
-      </div>
-    </div>
+  {/* Bottom Row */}
+  <div className="text-xs mt-1 opacity-80">
+    Created by <span className="font-semibold">Gunjan Jalan</span>, 
+    <span className="font-semibold"> Ashika</span>, and 
+    <span className="font-semibold"> Shrey Sojitra</span>
+  </div>
+
+</div>
+
   )
 }
